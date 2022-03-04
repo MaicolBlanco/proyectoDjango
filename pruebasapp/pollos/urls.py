92294f0ed67a398a2d534801b7a5236ae7079ccc
Maicol Blanco <maicolblancoalvarez@gmail.com>
2022-03-04 09:04:27 -0500

    Se realizan cambios en el proyecto

--- pruebasapp/pollos/urls.py
+++ pruebasapp/pollos/urls.py
@@ -3,6 +3,15 @@ from django.urls import path
 
 from . import views
 
+app_name = 'pollos'
 urlpatterns = [
-    path('', views.index, name='index')
+    # ex: /pollos/
+    path('', views.index, name='index'),
+    # ex: /pollos/5/
+    path('<int:question_id>/detail/pruebas_para_validar',
+         views.detail, name='detail'),
+    # ex: /pollos/5/results
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /pollos/5/vote
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- pruebasapp/pollos/views.py
+++ pruebasapp/pollos/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
 # Create your views here.
 from django.http import HttpResponse
 
+from .models import Question
+
 
 def index(request):
-    return HttpResponse('Hola Martin Y Esteban')
+    latest_question_list = Question.objects.all()
+    return render(request, 'pollos/index.html', {
+        "latest_question_list": latest_question_list
+    })
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'pollos/detail.html', {
+        'question': question
+    })
+
+
+def results(request, question_id):
+    return HttpResponse(f'Estas viendo los resultados de la pregunta número {question_id}')
+
+
+def vote(request, question_id):
+    return HttpResponse(f'Estas votando a la pregunta número {question_id}')
